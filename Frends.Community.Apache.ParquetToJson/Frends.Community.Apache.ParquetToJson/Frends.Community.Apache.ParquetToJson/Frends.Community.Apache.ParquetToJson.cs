b34c8180b87803790820cdfbca7e313d562163c3
Anssi Lumme <anssi.lumme@frends.com>
2025-04-30 14:05:46 +0300

    Created new solution.
    Created new solution from template for new task.

---
+++ Frends.Community.Apache.ParquetToJson/Frends.Community.Apache.ParquetToJson/Frends.Community.Apache.ParquetToJson/Frends.Community.Apache.ParquetToJson.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using Microsoft.CSharp; // You can remove this if you don't need dynamic type in .NET Standard frends Tasks
+
+#pragma warning disable 1591
+
+namespace Frends.Community.Apache.ParquetToJson
+{
+    public static class ParquetToJsonTask
+    {
+        /// <summary>
+        /// This is task
+        /// Documentation: https://github.com/CommunityHiQ/Frends.Community.Apache.ParquetToJson
+        /// </summary>
+        /// <param name="input">What to repeat.</param>
+        /// <param name="options">Define if repeated multiple times. </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>{string Replication} </returns>
+        public static Result ParquetToJson(Parameters input, [PropertyTab] Options options, CancellationToken cancellationToken)
+        {
+            var repeats = new string[options.Amount];
+
+            for (var i = 0; i < options.Amount; i++)
+            {
+                // It is good to check the cancellation token somewhere you spend lot of time, e.g. in loops.
+                cancellationToken.ThrowIfCancellationRequested();
+
+                repeats[i] = input.Message;
+            }
+
+            var output = new Result
+            {
+                Replication = string.Join(options.Delimiter, repeats)
+            };
+
+            return output;
+        }
+    }
+}
